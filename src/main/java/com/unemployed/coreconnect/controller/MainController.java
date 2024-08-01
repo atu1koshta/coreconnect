3ef400d7bcde1c4b2651fefce5dc33febb972d46
atul-koshta <atulk2018@gmail.com>
2024-08-01 18:40:01 +0530

    Configure security - configure spring security in WebSecurityConfig class - add MainController to redirect url to pages - add pages: index.html, login.html

--- src/main/java/com/unemployed/coreconnect/controller/MainController.java
+++ src/main/java/com/unemployed/coreconnect/controller/MainController.java
@@ -3,11 +3,23 @@ package com.unemployed.coreconnect.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
+
 @Controller
 public class MainController {
+    @GetMapping("/")
+    public String homePage() {
+        return "redirect:/index.html";
+    }
+    
 
     @GetMapping("/conversation")
-    public String redirectToConversation() {
+    public String conversationPage() {
         return "redirect:/conversation.html";
+    }
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "redirect:/login.html";
     }
 }
