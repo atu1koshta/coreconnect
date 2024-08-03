2867aab02bab429756848784350e79d692ce4f1a
atul-koshta <atulk2018@gmail.com>
2024-08-03 06:01:17 +0530

    Encode password before saving

--- src/main/java/com/unemployed/coreconnect/service/UserService.java
+++ src/main/java/com/unemployed/coreconnect/service/UserService.java
@@ -1,18 +1,25 @@
 package com.unemployed.coreconnect.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.unemployed.coreconnect.model.entities.User;
 import com.unemployed.coreconnect.repository.UserRepository;
 
+import jakarta.transaction.Transactional;
+
 @Service
 public class UserService {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Autowired
     private UserRepository userRepository;
 
+    @Transactional
     public User saveUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 
